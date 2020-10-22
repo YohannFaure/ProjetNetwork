d3ff1e0c9cec0c1fd61b1dffc0f003a04f791026
fgaudilliere <61799496+fgaudilliere@users.noreply.github.com>
2020-10-22 11:28:54 +0200

    Update Network_Analysis.py

--- Network_Analysis.py
+++ Network_Analysis.py
@@ -18,6 +18,20 @@ sorted(G.degree, key=lambda x: x[1], reverse=True)[:20]
 
 ### Degree Distribution
 
+degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
+degreeCount = collections.Counter(degree_sequence)
+deg, cnt = zip(*degreeCount.items())
+
+fig, ax = plt.subplots()
+plt.bar(deg, cnt, width=0.80, color="b")
+
+plt.title("Degree Histogram")
+plt.ylabel("Count")
+plt.xlabel("Degree")
+ax.set_xticks([d + 0.4 for d in deg])
+ax.set_xticklabels(deg)
+plt.show()
+
 
 ### Nodes of higher betweenness
 
