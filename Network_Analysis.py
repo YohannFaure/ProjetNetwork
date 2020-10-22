00f546256d142d673952a7884d7822641f141c15
faure.yohann <faure.yohann@gmail.com>
2020-10-22 13:24:47 +0200

    first day ended

--- Network_Analysis.py
+++ Network_Analysis.py
@@ -4,21 +4,37 @@ import numpy as np
 import collections
 
 def degree_distribution(G):
+    """
+    """
+    degrees = sorted([(d,n) for n, d in G.degree()], reverse=True)  # degree sequence
+    return(degrees)
 
-  degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
-  degreeCount = collections.Counter(degree_sequence)
-  deg, cnt = zip(*degreeCount.items())
+def Degree_distribution_plot(G):
+    """
+    """
+    degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
+    degreeCount = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degreeCount.items())
+    fig, ax = plt.subplots()
+    plt.bar(deg, cnt, width=0.80, color="b")
+    plt.title("Degree Histogram")
+    plt.ylabel("Count")
+    plt.xlabel("Degree")
+    ax.set_xticks([d + 0.4 for d in deg])
+    ax.set_xticklabels(deg)
+    plt.show()
+    return(None)
 
-  fig, ax = plt.subplots()
-  plt.bar(deg, cnt, width=0.80, color="b")
 
-  plt.title("Degree Histogram")
-  plt.ylabel("Count")
-  plt.xlabel("Degree")
-  ax.set_xticks([d + 0.4 for d in deg])
-  ax.set_xticklabels(deg)
-  plt.show()
-  
-  nodes_by_degree = [t[0] for t in sorted(G.degree, key=lambda x: x[1], reverse=True)]
-  
-  return(nodes_by_degree, degree_sequence)
+def degree_cut(G,mindeg,degrees=None):
+    GG=G.copy
+    if not degrees:
+        degrees = degree_distribution(GG)
+    i=0
+    while degrees[i][0]>mindeg:
+        i+=1
+    GG.remove_nodes_from([degrees[j][1] for j in range(i,len(degrees))])
+    return(GG)
+
+def GraphDraw(G):
+
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ The goal here is to make some data analysis on graphs. The graph we will focus o
 D.J. Watts and S.H. Strogatz, "Collective dynamics of 'small-world' networks." Nature 393, 440-442 (1998)., http://www.nature.com/nature/journal/v393/n6684/abs/393440a0.html
 ```
 
+Our **main goal** is to design an attack on the power grid in order to cut power to as many nodes as possible with as little efforts as possible.
+
+The idea behind such an attack preparation is to show the discrepancies of the system, and to create a protection or redundancy plan.
+
 ## File conversion
 
 We will first work on the data conversion, _i.e._ converting the original file type (_.gml_) into some NetworkX understandable data.
@@ -16,4 +20,8 @@ Lucky us, it is a simple enough task, networkx has a built-in function to do tha
 G=nx.read_gml("power.gml",label='id')
 ```
 
-> **Note:** This was too simple, so we decided to create a tool to read `.tsv` files. This could be useful if we ever need to read a `.tsv` graph at some point. The module associated with that is named `ReadTSV.py`.
+> **Note:** We decided to create a tool to read `.tsv` files, as our first network choice was different. This could be useful if we ever need to read a `.tsv` graph at some point. The module associated with that is named `ReadTSV.py`. The graph associated can be found [here](http://snap.stanford.edu/data/soc-RedditHyperlinks.html)
+
+## Information gathering
+
+We need to find information about the graph.
